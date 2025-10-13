b5d2fbe05d134895832480e55f5fd388e756cc57
Pedro4064 <pedrohlcruz@gmail.com>
2025-10-12 23:20:56 -0300

    [VERIFIED] correct pid algo

--- src/pid.c
+++ src/pid.c
@@ -8,24 +8,12 @@
 
 /*@
 
-    logic float integral(float a, float b, float delta_x) = 
-        (float)(delta_x * ((a + b)/2));
-
     predicate float_finite_and_in_range(float val, float low_bound, float up_bound) = 
         \is_finite(val) && (low_bound <= val <= up_bound);
 
 */
 
-/*@ 
-    
-  logic float bounded_integrator(pid_controller* pid_con, float old_error_integral, float previous_error_value) =
-        (old_error_integral + integral(pid_con->error_value, previous_error_value, pid_con->Ts) > pid_con->error_integral_ub) ? pid_con->error_integral_ub :
-        (old_error_integral + integral(pid_con->error_value, previous_error_value, pid_con->Ts) < pid_con->error_integral_lb) ? pid_con->error_integral_lb :
-        (float)(old_error_integral + integral(pid_con->error_value, previous_error_value, pid_con->Ts));
-*/
-
-
-/*@ 
+/*@
     requires valid_pointer: \valid(pid_con); 
     requires valid_Ts: float_finite_and_in_range(pid_con->Ts, 0.01f, 1.0f);
     requires valid_previous_error: float_finite_and_in_range(previous_error_value, (float)-3300, (float)3300);
@@ -38,18 +26,6 @@
     ensures (float)pid_con->error_integral_lb <= (float)pid_con->error_integral <= (float)pid_con->error_integral_ub;
     ensures \is_finite(pid_con->error_integral);
 
-    behavior integrator_ub:
-        assumes pid_con->error_integral + integral(pid_con->error_value, previous_error_value, pid_con->Ts) > pid_con->error_integral_ub;
-        ensures pid_con->error_integral == pid_con->error_integral_ub;
-    behavior integrator_lb:
-        assumes pid_con->error_integral + integral(pid_con->error_value, previous_error_value, pid_con->Ts) < pid_con->error_integral_lb;
-        ensures pid_con->error_integral == pid_con->error_integral_lb;
-    behavior integrator_ok:
-        assumes pid_con->error_integral + integral(pid_con->error_value, previous_error_value, pid_con->Ts) <= pid_con->error_integral_ub &&
-                pid_con->error_integral + integral(pid_con->error_value, previous_error_value, pid_con->Ts) >= pid_con->error_integral_lb;
-        ensures pid_con->error_integral == \old(pid_con->error_integral) + integral(pid_con->error_value, previous_error_value, pid_con->Ts);
-    complete behaviors;
-    disjoint behaviors;
 */
 void pid_integral_error(pid_controller* pid_con, float previous_error_value){
     float current_integral = pid_con->Ts * ((previous_error_value + pid_con->error_value)/2);
@@ -60,7 +36,6 @@ void pid_integral_error(pid_controller* pid_con, float previous_error_value){
     else 
         pid_con->error_integral += current_integral;
 }
-
 
 /*@
     requires valid_pointer: \valid(pid_con);
@@ -75,23 +50,6 @@ void pid_integral_error(pid_controller* pid_con, float previous_error_value){
     requires valid_integrator_ub: float_finite_and_in_range(pid_con->error_integral_ub, 0.0f, (float)(2000.0f*0.5f*3300.0f));
     requires valid_integrator_lb: float_finite_and_in_range(pid_con->error_integral_lb, (float)((-20.0f/0.01)*0.5f*(float)MAX_VOLTAGE), 0.0f);
     requires valid_integrator_error: float_finite_and_in_range(pid_con->error_integral, pid_con->error_integral_lb, pid_con->error_integral_ub);
-
-    assigns pid_con->actuator_effort;
-    assigns pid_con->error_value;
-    assigns pid_con->error_integral;
-
-    ensures pid_con->error_value == pid_con->target_value - pid_con->controlled_value;
-    ensures pid_con->error_integral == bounded_integrator(pid_con, \old(pid_con->error_integral), \old(pid_con->error_value));
-    ensures 0.0f<=pid_con->actuator_effort <= pid_con->controller_saturation;
-    ensures \let e = (pid_con->target_value - pid_con->controlled_value);
-            \let p = pid_con->kp * e;
-            \let d = pid_con->kd * ((e - \old(pid_con->error_value)) / pid_con->Ts);
-            \let i = pid_con->ki * bounded_integrator(pid_con, \old(pid_con->error_integral), \old(pid_con->error_value));
-            \let effort = (float)(p + i + d);
-            (pid_con->actuator_effort == ((effort >= pid_con->controller_saturation) ? pid_con->controller_saturation : 
-                                         ((effort < 0.0f) ? 0.0f : effort)));
-
-
 */
 void pid_compute_actuator_command(pid_controller* pid_con){
     float previous_error_value = pid_con->error_value;
@@ -140,8 +98,14 @@ void pid_compute_actuator_command(pid_controller* pid_con){
     //@ assert \is_finite(pid_con->actuator_effort);
     //@ assert \is_finite(integral_contribution);
 
-    pid_con->actuator_effort += integral_contribution;
-    // assert integ_contr_act_eff_range:  deriv_contr_lb+prop_contr_lb+integ_contr_lb<=pid_con->actuator_effort<=deriv_contr_ub+prop_contr_ub+integ_contr_ub;
+    //@ assert deriv_contr_lb+prop_contr_lb<=pid_con->actuator_effort<=deriv_contr_ub+prop_contr_ub;
+    //@ assert deriv_contr_lb+prop_contr_lb+integ_contr_lb <= pid_con->actuator_effort + integral_contribution<=deriv_contr_ub+prop_contr_ub+integ_contr_ub;
+    //@ assert -1e10f <= pid_con->actuator_effort <= 1e10f;
+    //@ assert -1e10f <= integral_contribution <= 1e10f;
+    float total_actuator_effort = pid_con->actuator_effort + integral_contribution;
+    //@ assert deriv_plus_int_contr_act_eff_range: (float)(deriv_contr_lb+prop_contr_lb+integ_contr_lb) <=total_actuator_effort<=(float)(deriv_contr_ub+prop_contr_ub+integ_contr_ub);
+
+    pid_con->actuator_effort = total_actuator_effort;
 
     // pid_con->actuator_effort = (pid_con->actuator_effort>=pid_con->controller_saturation)?
                                     // pid_con->controller_saturation:MAX(pid_con->actuator_effort, 0.0f);
